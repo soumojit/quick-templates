fa83e74c83d79e038e74076944bc4f9f9c9ca8cf
Soumojit Ghosh <tech.soumojit@gmail.com>
2020-11-15 14:12:40 +0530

    [init] - local db access from docker container

--- fast_api/app/models.py
+++ fast_api/app/models.py
@@ -5,10 +5,15 @@ USERNAME = 'tmp_user'
 PASSWORD = 'tmp_user@1234'
 AUTH_DB = 'admin'
 
+## [macOS - db on localhost, app on local]
+# HOST = 'localhost' or '127.0.0.1'
+## [macOS - db on localhost, app in docker]
+HOST = 'docker.for.mac.localhost'
+
 import datetime
 from mongoengine import connect, Document, StringField, DateTimeField
 
-connect(DATABASE, username=USERNAME, password=PASSWORD, authentication_source=AUTH_DB)
+connect(DATABASE, host=HOST, username=USERNAME, password=PASSWORD, authentication_source=AUTH_DB)
 
 class Post(Document):
     title = StringField(required=True, max_length=200)
